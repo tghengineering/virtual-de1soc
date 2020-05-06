77742d0c26dc1e73211cdf84cbc141d03678374b
Thomas Harper <tghengineering@gmail.com>
2020-05-07 02:18:24 +1000

    linux modelsim path added

--- src/config.py
+++ src/config.py
@@ -9,12 +9,11 @@ class Config():
 		self.lib_target_path = ""
 		self.lib_name = "work"
 		self.lib_top_level_entity = "MyProject"
-		# for windows 
-		#if os.name == 'nt': 
-		#	self.modelsim_path = "C:\\intelFPGA_lite\\18.1\\modelsim_ase\\win32aloem\\"
-		# for mac and linux(here, os.name is 'posix') 
-		#else: 
-			## TODO UPDATE TO VM BOX 
-		#	self.modelsim_path = "\\home\\ff\\intelFPGA_lite\\18.1\\modelsim_ase\\"
+		for windows 
+		if os.name == 'nt': 
+			self.modelsim_path = "C:\\intelFPGA_lite\\18.1\\modelsim_ase\\win32aloem\\"
+		 for mac and linux(here, os.name is 'posix') 
+		else:  
+			self.modelsim_path = "\\home\\ff\\intelFPGA_lite\\18.1\\modelsim_ase\\linuxaloem"
 
 		
