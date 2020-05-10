bf1b9c8e45b1d319310a29d2fa74c873986b51d7
Ha Jin Song <hSong6230@gmail.com>
2020-05-10 13:19:34 +1000

    Allow users to modify configuration prior to running the simulation

--- src/virtual_de1soc.py
+++ src/virtual_de1soc.py
@@ -7,12 +7,39 @@ import time
 import keyboard
 import pathlib
 
-def run_simulation(config):
+def initialise():
+	configuration = config.configuration
+
+	for index, key in enumerate(configuration):
+		print("{0}) {1}: {2}".format(index+1, key, configuration[key]))
+
+	userInput = input("Would you like to change any of these configuration? - Type in name of configuration or press ENTER to skip: ")
+
+	while(userInput != ""):
+		if userInput in configuration :
+			newConfigValue = input("Changing {0} to: ".format(userInput))
+			if newConfigValue != "":
+				configuration[userInput] = newConfigValue
+			else:
+				print("Cannot change confinguration to blank")
+		else:
+			print("Invalid configuration key")
+		userInput = input("Would you like to change any other configuration? - Number to select or press ENTER to skip: ")
+
+	print("New configurations are")
+
+	for index, key in enumerate(configuration):
+		print("{0}) {1}: {2}".format(index+1, key, configuration[key]))
+
+	return configuration
+
+
+def run_simulation(configuration):
 	board = fpga.Board()
 
 	screen = ascii_ui.BoardWriter()
 
-	sim = modelsim.BoardSimulator(board,config)
+	sim = modelsim.BoardSimulator(board, configuration)
 	#Seconds delay
 
 
@@ -25,12 +52,12 @@ def run_simulation(config):
 
 		screen.render(board)
 
-		for x in range(len(config["SW_key"])):
-			if keyboard.is_pressed(str(config["SW_key"][x])):
+		for x in range(len(configuration["SW_key"])):
+			if keyboard.is_pressed(str(configuration["SW_key"][x])):
 				board.SW.value[x] = not(board.SW.value[x])
 
-		for x in range(len(config["KEY_key"])):
-			if keyboard.is_pressed(str(config["KEY_key"][x])):
+		for x in range(len(configuration["KEY_key"])):
+			if keyboard.is_pressed(str(configuration["KEY_key"][x])):
 				board.KEY.value[x] = not(board.KEY.value[x])
 		board.SW.value[0]=not(board.SW.value[0])
 		board.CLOCK_50.value[0]=not(board.CLOCK_50.value[0])
@@ -38,7 +65,7 @@ def run_simulation(config):
 		sim.run("50ms")
 		
 		time_new = time.monotonic()
-		delay = config["time_delay"] - (time_new - time_old)
+		delay = configuration["time_delay"] - (time_new - time_old)
 		if (delay > 0):
 			time.sleep(delay)
 		#print("Time ahead of the loop (seconds): "+str(delay))
@@ -46,8 +73,7 @@ def run_simulation(config):
 			print("System lagging by: "+str(-delay))
 		time_old = time_new
 
+configuration = initialise()
 
-config = config.configuration
-
-run_simulation(config)
+#run_simulation(configuration)
 
